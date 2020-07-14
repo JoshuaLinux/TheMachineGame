2c9dd2bd6664698ea91694d0f30ff9c2372842c8
JoshuaLinux <68256671+JoshuaLinux@users.noreply.github.com>
2020-07-14 18:41:22 -0500

    Base logic for the game

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from pygame.locals import *
 import pygame
  
 class Player:
-    x = 300
-    y = 300
-    speed = 1
+    x = 44
+    y = 44
+    speed = 0.1
  
     def moveRight(self):
         self.x = self.x + self.speed
@@ -19,40 +19,71 @@ class Player:
  
     def moveDown(self):
         self.y = self.y + self.speed
-        
-class App:
-     
-    windowWidth = 1366
-    windowHeight = 768
-    player = 0
  
+class Maze:
+    def __init__(self):
+       self.M = 10
+       self.N = 8
+       self.maze = [ 1,1,1,1,1,1,1,1,1,1,
+                     1,0,0,0,0,0,0,0,0,1,
+                     1,0,0,0,0,0,0,0,0,1,
+                     1,0,1,1,1,1,1,1,0,1,
+                     1,0,1,0,0,0,0,0,0,1,
+                     1,0,1,0,1,1,1,1,0,1,
+                     1,0,0,0,0,0,0,0,0,1,
+                     1,1,1,1,1,1,1,1,1,1,]
+
+    def draw(self,display_surf,image_surf):
+       bx = 0
+       by = 0
+       for i in range(0,self.M*self.N):
+           if self.maze[ bx + (by*self.M) ] == 1:
+               display_surf.blit(image_surf,( bx * 44 , by * 44))
+
+           bx = bx + 1
+           if bx > self.M-1:
+               bx = 0
+               by = by + 1
+
+
+class App:
+
+    windowWidth = 1400
+    windowHeight = 600
+    player = 0
+
     def __init__(self):
         self._running = True
         self._display_surf = None
         self._image_surf = None
+        self._block_surf = None
         self.player = Player()
- 
+        self.maze = Maze()
+
     def on_init(self):
         pygame.init()
-        self._display_surf = pygame.display.set_mode((self.windowWidth,self.windowHeight), pygame.HWSURFACE)
-        
-        pygame.display.set_caption('Crystal Test')
+        self._display_surf = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
+
+        pygame.display.set_caption('Pygame pythonspot.com example')
         self._running = True
-        self._image_surf = pygame.image.load("Crystal.png").convert()
- 
+        self._image_surf = pygame.image.load("SmallCrystal.png").convert()
+        self._block_surf = pygame.image.load("Wumpa.png").convert()
+
     def on_event(self, event):
         if event.type == QUIT:
             self._running = False
- 
+
     def on_loop(self):
         pass
-    
+
     def on_render(self):
         self._display_surf.fill((0,0,0))
         self._display_surf.blit(self._image_surf,(self.player.x,self.player.y))
+        self.maze.draw(self._display_surf, self._block_surf)
         pygame.display.flip()
  
     def on_cleanup(self):
+        pygame.display.quit()
         pygame.quit()
  
     def on_execute(self):
